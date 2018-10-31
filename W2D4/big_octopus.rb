93d5e8885e4876eaaf2e846b429166b817a2d27a
gwang <guanw88@gmail.com>
2018-10-30 22:41:22 -0700

    Add o(n) #slow_dance and o(1) #fast_dance methods

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -48,21 +48,34 @@ end
 # Full of fish, the Octopus attempts Dance Dance Revolution. The game has tiles in the following directions:
 #
 # tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
-# To play the game, the octopus must step on a tile with her corresponding tentacle. We can assume that the octopus's eight tentacles are numbered and correspond to the tile direction indices.
-#
+# To play the game, the octopus must step on a tile with her corresponding tentacle.
+# We can assume that the octopus's eight tentacles are numbered and correspond to the tile direction indices.
+
+
 # Slow Dance
-# Given a tile direction, iterate through a tiles array to return the tentacle number (tile index) the octopus must move. This should take O(n) time.
+# Given a tile direction, iterate through a tiles array to return the tentacle
+# number (tile index) the octopus must move. This should take O(n) time.
 #
 # slow_dance("up", tiles_array)
 # > 0
 #
 # slow_dance("right-down", tiles_array)
 # > 3
+
+def slow_dance(direction, tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up"])
+  tiles_array.each_with_index {|dir, idx| return idx if direction == dir}
+end
+
 # Constant Dance!
-# Now that the octopus is warmed up, let's help her dance faster. Use a different data structure and write a new function so that you can access the tentacle number in O(1) time.
+# Now that the octopus is warmed up, let's help her dance faster. Use a different
+# data structure and write a new function so that you can access the tentacle number in O(1) time.
 #
 # fast_dance("up", new_tiles_data_structure)
 # > 0
 #
 # fast_dance("right-down", new_tiles_data_structure)
 # > 3
+
+def fast_dance(direction, hash = {"up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3, "down" => 4, "left-down" => 5, "left" => 6, "left-up" => 8})
+  hash[direction]
+end
