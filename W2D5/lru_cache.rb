f3077590954272bfae99b9555062e5076303a87e
gwang <guanw88@gmail.com>
2018-10-31 23:57:23 -0700

    Add #add and #show methods to LRUCache class

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # Tonight we're going to implement a basic LRU Cache using an array. This will be a
 # simple implementation that doesn't use a hash-map or linked list. We will assume
 # our input is limited to Fixnums, Strings, Arrays, Symbols, and Hashes. We will
@@ -5,27 +6,28 @@
 #
 # Ultimately, our LRU Cache should function as follows:
 #
-#   johnny_cache = LRUCache.new(4)
-#
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add(5)
-#
-#   johnny_cache.count # => returns 2
-#
-#   johnny_cache.add([1,2,3])
-#   johnny_cache.add(5)
-#   johnny_cache.add(-5)
-#   johnny_cache.add({a: 1, b: 2, c: 3})
-#   johnny_cache.add([1,2,3,4])
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add(:ring_of_fire)
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add({a: 1, b: 2, c: 3})
+  # johnny_cache = LRUCache.new(4)
+  #
+  # johnny_cache.add("I walk the line")
+  # johnny_cache.add(5)
+  #
+  # johnny_cache.count # => returns 2
+  #
+  # johnny_cache.add([1,2,3])
+  # johnny_cache.add(5)
+  # johnny_cache.add(-5)
+  # johnny_cache.add({a: 1, b: 2, c: 3})
+  # johnny_cache.add([1,2,3,4])
+  # johnny_cache.add("I walk the line")
+  # johnny_cache.add(:ring_of_fire)
+  # johnny_cache.add("I walk the line")
+  # johnny_cache.add({a: 1, b: 2, c: 3})
 #
 #
 #   johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
 
 class LRUCache
+  attr_accessor :cache
   def initialize(size)
     @cache = Array.new(size, nil) #[nil, nil, nil, nil] with size = 4
   end
@@ -35,11 +37,16 @@ class LRUCache
   end
 
   def add(el)
-    # adds element to cache according to LRU principle
+    if is_el_present?(el)
+      @cache.reject!{|el_in_cache| el_in_cache == el}.push(el)
+    else
+      @cache.shift
+      @cache << el
+    end
   end
 
   def show
-    # shows the items in the cache, with the LRU item first
+    @cache
   end
 
   private
