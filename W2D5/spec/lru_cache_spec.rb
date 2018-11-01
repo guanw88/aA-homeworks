52032560f8bda6f9d2c8c33523a39cee591e39de
gwang <guanw88@gmail.com>
2018-11-01 10:05:20 -0700

    Add rspecs for #add method for LRU Cache

--- W2D5/spec/lru_cache_spec.rb
+++ W2D5/spec/lru_cache_spec.rb
@@ -28,7 +28,34 @@ RSpec.describe LRUCache do
   end
 
   describe "#add" do
-
+    context "when the element does not already exist in the cache" do
+      it "adds new elements to the cache" do
+        lrucache.add("I walk the line")
+        lrucache.add(5)
+        lrucache.add(:ring_of_fire)
+        lrucache.add({a: 1, b: 2, c: 3})
+        expect(lrucache.cache).to eq(["I walk the line", 5, :ring_of_fire, {a: 1, b: 2, c: 3}])
+      end
+    end
+    context "when the element already exists in the cache" do
+      it "does not add the element again" do
+        lrucache.add("I walk the line")
+        lrucache.add(5)
+        lrucache.add("I walk the line")
+        lrucache.add(:ring_of_fire)
+        expect(lrucache.cache).not_to eq(["I walk the line", 5, "I walk the line", :ring_of_fire])
+      end
+      it "changes the position of the element" do
+        lrucache.add("I walk the line")
+        lrucache.add("I walk the line")
+        expect(lrucache.cache[-1]).to eq("I walk the line")
+        expect(lrucache.cache[-2]).not_to eq("I walk the line")
+      end
+    end
   end
+
+  describe "#show" do
+
+  end 
 
 end
