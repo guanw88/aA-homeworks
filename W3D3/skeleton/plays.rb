ba2e8ea53e75528d365dde40890cb3d0f6158e88
gwang <guanw88@gmail.com>
2018-11-04 19:44:47 -0800

    Add Play::find_by_playwright method to find plays by playwright name in plays.db

--- W3D3/skeleton/plays.rb
+++ W3D3/skeleton/plays.rb
@@ -29,13 +29,13 @@ class Play
   end
 
   def self.find_by_playwright(name)
-    # data = PlayDBConnection.instance.execute(<<-SQL, title)
-    #   SELECT *
-    #   FROM plays
-    #   INNER JOIN
-    #   WHERE title = ?
-    # SQL
-    # data.map { |datum| Play.new(datum) }
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT *
+      FROM plays
+      INNER JOIN playwrights ON plays.playwright_id == playwrights.id
+      WHERE name = ?
+    SQL
+    data.map { |datum| Play.new(datum) }
   end
 
   def initialize(options)
@@ -86,17 +86,34 @@ class Playwright
     data.map { |datum| Playwright.new(datum) }
   end
 
-  def initialize(options) #why is this called #new?
+  def initialize(options)
     @id = options['id']
     @name = options['name']
     @birth_year = options['birth_year']
   end
 
-  def create
-  end
-
-  def update
-  end
+  # def create
+  #   raise "#{self} already in database" if self.id
+  #   PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+  #     INSERT INTO
+  #       playwrights (name, birth_year)
+  #     VALUES
+  #       (?, ?)
+  #   SQL
+  #   self.id = PlayDBConnection.instance.last_insert_row_id
+  # end
+  #
+  # def update
+  #   raise "#{self} not in database" unless self.id
+  #   PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+  #     UPDATE
+  #       playwrights
+  #     SET
+  #       name = ?, birth_year = ?
+  #     WHERE
+  #       id = ?
+  #   SQL
+  # end
 
   def get_plays #returns all plays written by playwright
   end
