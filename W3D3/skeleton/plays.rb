84dd51192812e62df622157a191eeed022407ea7
gwang <guanw88@gmail.com>
2018-11-04 19:21:47 -0800

    Add Playwright::find_by_name to search plays.db by playwright name

--- W3D3/skeleton/plays.rb
+++ W3D3/skeleton/plays.rb
@@ -78,9 +78,15 @@ class Playwright
   end
 
   def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT *
+      FROM playwrights
+      WHERE name = ?
+    SQL
+    data.map { |datum| Playwright.new(datum) }
   end
 
-  def new(options) #why is this called #new?
+  def initialize(options) #why is this called #new?
     @id = options['id']
     @name = options['name']
     @birth_year = options['birth_year']
