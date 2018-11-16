684d3d29e987d951e70c887269295ccd9406e0b1
gwang <guanw88@gmail.com>
2018-11-15 22:29:20 -0800

    Add rspec tests for User model methods; fix bug with nil values in User::find_by_credentials

--- W4D5/music_app/spec/models/user_spec.rb
+++ W4D5/music_app/spec/models/user_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) {User.new(
+                    email: "test@test.com",
+                    password: "password"
+                  )}
+
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
   it { should validate_length_of(:password).is_at_least(6) }
+
+  describe "#is_password?" do
+    it "should return true when a correct password is entered" do
+      expect(user.is_password?("password")).to be true
+    end
+    it "should return false when an incorrect password is submitted" do
+      expect(user.is_password?("wordpass")).to be false
+    end
+  end
+
+  describe "#reset_session_token" do
+    it "should generate a new session token" do
+      original_session_token = user.session_token
+      user.reset_session_token!
+      expect(user.session_token).to_not eq(original_session_token)
+    end
+  end
+
+  describe "::find_by_credentials" do
+    before {user.save}
+
+    it "should find a matching user" do
+      expect(User.find_by_credentials("test@test.com", "password")).to eq(user)
+    end
+    it "should return nil if no matching user found" do
+      expect(User.find_by_credentials("nomatch@test.com", "wordpass")).to_not eq(user)
+    end
+    it "should return nil if email or password do not match" do
+      expect(User.find_by_credentials("test@test.com", "wordpass")).to_not eq(user)
+      expect(User.find_by_credentials("nomatch@test.com", "password")).to_not eq(user)
+    end
+  end
+
 end
