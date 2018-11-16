27c3074325e53b096b8c869e7e91b106c192ecb1
gwang <guanw88@gmail.com>
2018-11-15 21:47:38 -0800

    Fix bug in password validation and create User model validation specs

--- W4D5/music_app/app/models/user.rb
+++ W4D5/music_app/app/models/user.rb
@@ -12,7 +12,7 @@
 
 class User < ApplicationRecord
   validates :email, :session_token, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6, allow_nil: true}
+  validates :password, length: {minimum: 6, allow_nil: true}
   validates :password_digest, presence: true
 
   after_initialize :ensure_session_token
@@ -44,6 +44,7 @@ class User < ApplicationRecord
   end
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
