55b2c9cbd1d1e9013c815f508208d99d12aa7275
gwang <guanw88@gmail.com>
2018-11-18 23:57:09 -0800

    Create toys table in db with polymorphic fields

--- W5D2/skeleton/app/models/toy.rb
+++ W5D2/skeleton/app/models/toy.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :bigint(8)
+#
+
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
