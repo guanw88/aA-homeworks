7d9fe34463945f7b7c31fe2a8728d73ce3ec2aab
gwang <guanw88@gmail.com>
2018-11-25 20:20:43 -0800

    Set up canvas and add blue rectangle

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  // Set up canvas dimensions as 500px by 500px
+  const canvas = document.getElementById('myCanvas');
+  canvas.width = 500;
+  canvas.heigth = 500;
+  ctx = canvas.getContext('2d');
+
+  // Create blue rectangle
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(10, 10, 100, 100);
 });
