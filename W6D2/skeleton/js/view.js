b2fe82d75c0b2041154a7378f6a8f3fbd47dd0c5
gwang <guanw88@gmail.com>
2018-11-25 21:45:41 -0800

    Complete jQuery exercises

--- W6D2/skeleton/js/view.js
+++ W6D2/skeleton/js/view.js
@@ -22,28 +22,28 @@ View.prototype.exercise1 = function () {
   //Challenge: Give every square the class 'orange'
   //Result: Every square should turn orange (we already have a CSS rule)
 
-  $('.square').addClass("orange");
+  $(".square").addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
   //Challenge: Remove every square
   //Result: Every square vanishes
 
-  //your code here!
+  $(".square").remove();
 };
 
 View.prototype.exercise3 = function () {
   //Challenge: Add an <h1> with the text 'i love jquery' under the grid.
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
-  //your code here!
+  $("#easel").append("<h1>i love jquery</h1>");
 };
 
 View.prototype.exercise4 = function () {
   //Challenge: Write your first name in every other square.
   //Result: Your name appears in every other square.
 
-  //your code here!
+  $("#easel li:nth-child(2n)").append("<p>George</p>");
 };
 
 View.prototype.exercise5 = function () {
@@ -54,7 +54,9 @@ View.prototype.exercise5 = function () {
   //hint: checkout the addRow function at the bottom of the file: we set the
   //  'data-pos' of every square
 
-  //your code here!
+  $( "li" ).on( "click", function() {
+    alert( $(this).attr("data-pos") );
+  });
 };
 
 View.prototype.exercise6 = function () {
@@ -64,7 +66,10 @@ View.prototype.exercise6 = function () {
 
   //hint: use window._randomColorString() (defined at top) to get a random color!
 
-  //your code here!
+  $(".square").each( function() {
+    $(this).css('background-color', window._randomColorString());
+  });
+
 };
 
 View.prototype.exercise7 = function(){
@@ -74,7 +79,9 @@ View.prototype.exercise7 = function(){
   //You should push the button for exercise 6 first to try it on the
   //rainbow.
 
-  //your code here!
+  $( "li" ).hover( function() {
+    console.log( $(this).css("background-color") );
+  });
 };
 
 
