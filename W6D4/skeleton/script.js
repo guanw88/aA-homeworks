325cb06a90fba9b2ffc7ed6f2a5c92aabfcaf764
gwang <guanw88@gmail.com>
2018-11-29 00:26:04 -0800

    Update broken image link in HTML and add functionality to toggle photo add form

--- W6D4/skeleton/script.js
+++ W6D4/skeleton/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   // toggling restaurants
-
   const toggleLi = (e) => {
     const li = e.target;
     if (li.className === "visited") {
@@ -14,25 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
-
   // adding SF places as list items
+  const addLi = (e) => {
+    e.preventDefault();
+    const ul = document.getElementById("sf-places");
+    const li = document.createElement("li");
+    const textField = document.getElementsByClassName("favorite-input")[0];
+    li.textContent = textField.value;
+    ul.appendChild(li);
+  };
 
- const addLi = (e) => {
-   e.preventDefault();
-   const ul = document.getElementById("sf-places");
-   const li = document.createElement("li");
-   const textField = document.getElementsByClassName("favorite-input")[0];
-   li.textContent = textField.value;
-   ul.appendChild(li);
- };
+  document.querySelector(".favorite-submit").addEventListener("click", addLi);
 
- document.querySelector(".favorite-submit").addEventListener("click", addLi);
+  // toggle photo add form
+  const togglePhotoForm = (e) => {
+    debugger
+    const photoForm = document.getElementsByClassName("photo-form-container")[0]
+    if (photoForm.classList.contains("hidden")){
+      photoForm.classList.remove("hidden");
+    } else {
+      photoForm.classList.add("hidden");
+    }
+  };
 
-
+  document.querySelector(".photo-show-button").addEventListener("click", togglePhotoForm);
 
   // adding new photos
-
-  // --- your code here!
 
 
 
