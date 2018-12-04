df0ee5ec1b0210189593485f8fc9667cc4973004
gwang <guanw88@gmail.com>
2018-12-03 22:33:51 -0800

    Add custom middleware method and refactor

--- W7D2/solution_1/frontend/todo_redux.jsx
+++ W7D2/solution_1/frontend/todo_redux.jsx
@@ -4,10 +4,43 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+// Phase 1
+// const addLoggingToDispatch = (store) => {
+//   let storeDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     storeDispatch(action);
+//     console.log(store.getState());
+//   }
+// };
+
+// Phase 2
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach( (middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch });
+// };
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  // Phase 1
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // Phase 2
+  // store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
