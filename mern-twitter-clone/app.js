219074bc6794fc3812c47429f3e925cfd84c03fc
gwang <guanw88@gmail.com>
2019-01-01 00:02:09 -0800

    Adjust app.js and package.json to prep for Heroku deployment

--- mern-twitter-clone/app.js
+++ mern-twitter-clone/app.js
@@ -5,6 +5,14 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
